973bfca780b58412b17d80c2da42d81e938fad51
Vikash <empirevikash@gmail.com>
2023-10-13 22:17:01 +0530

    justStarting

---
+++ src/Components/Headers/Header.js
@@ -0,0 +1,12 @@
+import './header.css';
+
+function Header() {
+
+  return (
+    <div className='navbar'>
+      <p className='logo' >Finance Tracker.</p>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Pages/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Components/Headers/Header'
+
+function Signup() {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default Signup
